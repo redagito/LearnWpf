817e6ed7e556606173db8dac2dfbe8be595aed69
Robert J <agitodev@gmail.com>
2024-05-20 19:20:11 +0200

    Work on customerdemo

--- LearnWpf.CustomerDemo/Domain/ViewModels/MainViewModel.cs
+++ LearnWpf.CustomerDemo/Domain/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using LearnWpf.CustomerDemo.DataAccess.Models;
 using LearnWpf.CustomerDemo.Domain.Services;
 using System.Collections.ObjectModel;
@@ -13,7 +14,37 @@ namespace LearnWpf.CustomerDemo.Domain.ViewModels
         private ObservableCollection<Customer> _customers = new();
 
         [ObservableProperty]
-        private Customer _selectedCustomer = new();
+        [NotifyCanExecuteChangedFor(nameof(DeleteCustomerCommand))]
+        private Customer? _selectedCustomer = null;
+
+        /// <summary>
+        /// Creates new customer, reloads customer list and sets currently selected customer to newly created customer
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [RelayCommand]
+        private async Task NewCustomerAsync(CancellationToken cancellationToken)
+        {
+            var customer = await _customerRepository.CreateCustomerAsync(new Customer(), cancellationToken);
+            await ReloadCustomers(cancellationToken);
+            SelectedCustomer = customer;
+        }
+
+        private bool DeleteCustomerCanExecute() => SelectedCustomer != null;
+
+        [RelayCommand(CanExecute = nameof(DeleteCustomerCanExecute))]
+        private async Task DeleteCustomerAsync(CancellationToken cancellationToken) 
+        {
+            if (SelectedCustomer == null) return;
+
+            await _customerRepository.DeleteCustomerAsync(SelectedCustomer.Id, cancellationToken);
+            await ReloadCustomers(cancellationToken);
+        }
+
+        private async Task ReloadCustomers(CancellationToken cancellationToken)
+        {
+            Customers = new ObservableCollection<Customer>(await _customerRepository.GetCustomersAsync(cancellationToken));
+        }
 
         public MainViewModel(ICustomerRepository customerRepository)
         {
